f8a18c9b7c1b91b1fdc3ae73125f37e25d542896
andrezinho <110039135+AndreKaled@users.noreply.github.com>
2022-09-25 15:52:43 -0400

    adicionando modelos e separando os componentes em pacotes

---
+++ Dindin-/src/view/ModoDeJogo.java
@@ -0,0 +1,172 @@
+package view;
+
+/**André Kaled Duarte - 18/09/2022
+ * Kauan Ferreira dos Santos - 18/09/2022
+ * Graziela da Costa Ralph - 20/09/2022
+ * 
+ * Modo de Jogo
+ * Classe responsável pela escolha dentre os dois modos de jogo: Fácil e Difícil.
+ * essa classe dispara um evento ao clique do usuário em algum modo, onde deve 
+ * trocar a tela apresentando ao referido modo escolhido.*/
+
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class ModoDeJogo extends JPanel {
+	// declaração de variaveis dos componentes
+	private JButton panelFacil, panelDificil, panelVoltar;
+	private JPanel telaFacil = new ModoFacil(),
+			telaDificil = new ModoDificil();
+	private int contX = 0, contY = 0;
+	private JPanel c;
+	private static String nome = "MODO DE JOGO";
+	private ImageIcon imgFacil = new ImageIcon(getClass().getResource("modo facil.png")),
+			imgDificil = new ImageIcon(getClass().getResource("modo dificil.png")), 
+			imgBtVoltar = new ImageIcon(getClass().getResource ("botao voltar.png"));
+
+	// construtor da classe
+	public ModoDeJogo() {
+		setLayout(null);
+		setBackground(new Color(66, 153, 206));
+		panelVoltar = new JButton(imgBtVoltar);
+		panelFacil = new JButton(imgFacil);
+		panelDificil = new JButton(imgDificil);
+		c = new JPanel(null);
+		configuraPanels();
+		configuraBtVoltar();
+		eventos();
+
+		// nome do painel para mudar de tela no cardLayout
+		setName(nome);
+	}
+
+	// configurando painéis
+	private void configuraPanels() {
+		c.setBounds(20,80,1350,1200);
+		c.setBackground(new Color(66, 153, 206));
+		add(c);
+		
+		panelFacil.setBounds(0, 0, 650, 600);
+		panelDificil.setBounds(670, 0, 650, 600);
+		
+		panelFacil.setContentAreaFilled(false);
+		panelFacil.setBorderPainted(false);
+		panelDificil.setContentAreaFilled(false);
+		panelDificil.setBorderPainted(false);
+
+		c.add(panelFacil);
+		c.add(panelDificil);
+	}
+
+	/** configurando o botão de voltar */
+	private void configuraBtVoltar() {
+		add(panelVoltar);
+		panelVoltar.setBounds (10, 10, 50, 50);
+		panelVoltar.setContentAreaFilled(false);
+		panelVoltar.setBorderPainted(false);
+
+		panelVoltar.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				Menu.voltaTela();
+			}
+
+		});
+	}
+
+	/**
+	 * Tratando eventos de clique, é uma área de escolha de dois modos de jogo
+	 * então ao clicar em uma opção, a configuração deve alterar e a tela também
+	 */
+	private void eventos() {
+		panelFacil.addMouseListener(new MouseListener() {
+
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				// adicionando ao cardLayout
+				if (telaFacil == null || contY == 0) {
+					telaFacil = new ModoFacil();
+					Menu.adicionaTela(telaFacil, telaFacil.getName());
+					contY++;
+				}
+				// mudando tela
+				Menu.mudaTela(telaFacil.getName());
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+
+			}
+
+			@Override
+			public void mouseExited(MouseEvent arg0) {
+
+			}
+
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent arg0) {
+
+			}
+
+		});
+
+		panelDificil.addMouseListener(new MouseListener() {
+
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+
+				// adicionando ao cardLayout
+				if (telaDificil == null || contX == 0) {
+					telaDificil = new ModoDificil();
+					Menu.adicionaTela(telaDificil, telaDificil.getName());
+					contX++;
+				}
+				// mudando tela
+				Menu.mudaTela(telaDificil.getName());
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+
+			}
+
+			@Override
+			public void mouseExited(MouseEvent arg0) {
+
+			}
+
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent arg0) {
+
+			}
+
+		});
+	}
+
+	// metodo de acesso ao nome da classe
+	static protected String getNome() {
+		return nome;
+	}
+
+	// fim da classe
+
+}
---
+++ Dindin-/src/view/ModoFacil.java
@@ -0,0 +1,88 @@
+package view;
+
+/**André Kaled Duarte - 10/09/2022
+ * 
+ * Modo fácil
+ * essa classe deve ser um dos modos de jogo escolhido pelo usuário, as configurções do jogo são
+ * definidas para iniciantes porém a interface continua a mesma, aqui é onde será a area principal do jogo pois
+ * é onde os jogadores passarão mais tempo.
+ * 
+ * Dado? ok
+ * Tabuleiro? X
+ * Conectado posição do personagem? X
+ * Lista de compras do Jogador? X
+ * */
+
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import modelo.Dado;
+
+public class ModoFacil extends JPanel {
+
+	//declaração de atributos
+	private JPanel areaDado;
+	private JButton btRolar, btVoltar;
+	private JLabel resultado;
+
+	//construtor da classe
+	public ModoFacil() {
+		setLayout(null);
+		setBackground(new Color(66, 153, 206));
+		
+		areaDado = new JPanel();
+		btRolar = new JButton("ROLAR DADO");
+		btVoltar = new JButton("VOLTAR");
+		resultado = new JLabel("" + Dado.rolar());
+
+		configuraDado();
+		configuraBtVoltar();
+		
+		//nome para a troca de tela
+		setName("MODO FACIL");
+	}
+
+	// configurando area do Dado
+	private void configuraDado() {
+		areaDado.setBackground(Color.GRAY);
+		areaDado.setLayout(new FlowLayout());
+		areaDado.add(resultado);
+		areaDado.add(btRolar);
+		areaDado.setBounds(100, 100, 100, 100);
+		add(areaDado);
+
+		resultado.setFont(new Font("Arial", 1, 19));
+		resultado.setForeground(Color.ORANGE);
+		btRolar.setFont(new Font("Arial", 1, 14));
+
+		btRolar.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				resultado.setText("" + Dado.rolar());
+			}
+
+		});
+	}
+
+	// configurando botao de voltar
+	private void configuraBtVoltar() {
+		btVoltar.setBounds(10, 10, 100, 20);
+		add(btVoltar);
+		btVoltar.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				Menu.mudaTela(ModoDeJogo.getNome());
+			}
+
+		});
+	}
+}
