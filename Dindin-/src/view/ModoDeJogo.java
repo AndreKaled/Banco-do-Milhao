c51c6c1e52f5dc2d131dc72574b403f85c8c8446
kauan <kauan@KauanSantos.bbrouter>
2022-09-28 17:46:29 -0400

    Finalizando a tela de escolha de modos

--- Dindin-/src/view/ModoDeJogo.java
+++ Dindin-/src/view/ModoDeJogo.java
@@ -1,16 +1,6 @@
 package view;
 
-/**Andr� Kaled Duarte - 18/09/2022
- * Kauan Ferreira dos Santos - 18/09/2022
- * Graziela da Costa Ralph - 20/09/2022
- * 
- * Modo de Jogo
- * Classe respons�vel pela escolha dentre os dois modos de jogo: F�cil e Dif�cil.
- * essa classe dispara um evento ao clique do usu�rio em algum modo, onde deve 
- * trocar a tela apresentando ao referido modo escolhido.*/
-
 import java.awt.Color;
-import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
@@ -18,19 +8,27 @@ import java.awt.event.MouseListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class ModoDeJogo extends JPanel {
-	// declara��o de variaveis dos componentes
-	private JButton panelFacil, panelDificil, panelVoltar;
+
+	// declaração de variaveis dos componentes
+	private JButton panelFacil, panelDificil, panelVoltar, infoFacil, panelInfoFacil, infoDificil, panelInfoDificil;
 	private JPanel telaFacil = new ModoFacil(),
 			telaDificil = new ModoDificil();
+	private JLabel lbmodJogo;
 	private int contX = 0, contY = 0;
-	private JPanel c;
+	// private JPanel c;
 	private static String nome = "MODO DE JOGO";
 	private ImageIcon imgFacil = new ImageIcon(getClass().getResource("modo facil.png")),
-			imgDificil = new ImageIcon(getClass().getResource("modo dificil.png")), 
-			imgBtVoltar = new ImageIcon(getClass().getResource ("botao voltar.png"));
+			imgDificil = new ImageIcon(getClass().getResource("modo dificil.png")),
+			imgBtVoltar = new ImageIcon(getClass().getResource("botao voltar.png")),
+			imgTitulo = new ImageIcon(getClass().getResource("Modos de Jogo.png")),
+			imgInfoFacil = new ImageIcon(getClass().getResource("info roxo.png")),
+			imgPanelFacil = new ImageIcon(getClass().getResource("facil.png")),
+			imgInfoDificil = new ImageIcon(getClass().getResource("info verde.png")),
+			imgPanelDificil = new ImageIcon(getClass().getResource("dificil.png"));
 
 	// construtor da classe
 	public ModoDeJogo() {
@@ -39,37 +37,129 @@ public class ModoDeJogo extends JPanel {
 		panelVoltar = new JButton(imgBtVoltar);
 		panelFacil = new JButton(imgFacil);
 		panelDificil = new JButton(imgDificil);
-		c = new JPanel(null);
+		infoFacil = new JButton(imgInfoFacil);
+		lbmodJogo = new JLabel(imgTitulo);
+		panelInfoFacil = new JButton(imgPanelFacil);
+		infoDificil = new JButton(imgInfoDificil);
+		panelInfoDificil = new JButton(imgPanelDificil);
+
 		configuraPanels();
 		configuraBtVoltar();
+		configuraTitulo();
+		configuraBtInfo();
 		eventos();
 
 		// nome do painel para mudar de tela no cardLayout
 		setName(nome);
 	}
 
-	// configurando pain�is
+	public void configuraBtInfo() {
+		infoFacil.setLayout(null);
+		infoFacil.setBounds(100, 10, 700, 610);
+		infoFacil.setContentAreaFilled(false);
+		infoFacil.setBorderPainted(false);
+		infoFacil.setVisible(true);
+		panelFacil.add(infoFacil);
+
+		panelInfoFacil.setLayout(null);
+		panelInfoFacil.setVisible(false);
+		panelInfoFacil.setContentAreaFilled(false);
+		panelInfoFacil.setBorderPainted(false);
+		panelInfoFacil.setBounds(20, 80, 650, 600);
+		add(panelInfoFacil);
+
+		infoDificil.setLayout(null);
+		infoDificil.setBounds(700, 200, 700, 610);
+		infoDificil.setContentAreaFilled(false);
+		infoDificil.setBorderPainted(false);
+		infoDificil.setVisible(true);
+		panelDificil.add(infoDificil);
+
+		panelInfoDificil.setLayout(null);
+		panelInfoDificil.setVisible(false);
+		panelInfoDificil.setContentAreaFilled(false);
+		panelInfoDificil.setBorderPainted(false);
+		panelInfoDificil.setBounds(690, 80, 650, 600);
+		add(panelInfoDificil);
+
+		// configura o botao de informações do modo facil
+		infoFacil.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				panelFacil.setVisible(false);
+				infoFacil.setVisible(false);
+				panelInfoFacil.setVisible(true);
+
+			}
+
+		});
+
+		// configura a area de informações do modo facil
+		panelInfoFacil.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				panelInfoFacil.setVisible(false);
+				panelFacil.setVisible(true);
+				infoFacil.setVisible(true);
+
+			}
+
+		});
+
+		infoDificil.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				panelDificil.setVisible(false);
+				infoDificil.setVisible(false);
+				panelInfoDificil.setVisible(true);
+
+			}
+
+		});
+
+		panelInfoDificil.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				panelInfoDificil.setVisible(false);
+				panelDificil.setVisible(true);
+				infoDificil.setVisible(true);
+
+			}
+
+		});
+	}
+
+	// configurando painéis
 	private void configuraPanels() {
-		c.setBounds(20,80,1350,1200);
-		c.setBackground(new Color(66, 153, 206));
-		add(c);
-		
-		panelFacil.setBounds(0, 0, 650, 600);
-		panelDificil.setBounds(670, 0, 650, 600);
-		
+
+		panelFacil.setBounds(20, 80, 650, 600);
+		panelDificil.setBounds(690, 80, 650, 600);
+
 		panelFacil.setContentAreaFilled(false);
 		panelFacil.setBorderPainted(false);
 		panelDificil.setContentAreaFilled(false);
 		panelDificil.setBorderPainted(false);
 
-		c.add(panelFacil);
-		c.add(panelDificil);
+		add(panelFacil);
+		add(panelDificil);
+
 	}
 
-	/** configurando o bot�o de voltar */
+	// configura a imagem do titulo
+	private void configuraTitulo() {
+		add(lbmodJogo);
+		lbmodJogo.setBounds(350, 10, 700, 100);
+		setVisible(true);
+	}
+
+	/** configurando o botão de voltar */
 	private void configuraBtVoltar() {
 		add(panelVoltar);
-		panelVoltar.setBounds (10, 10, 50, 50);
+		panelVoltar.setBounds(10, 10, 70, 70);
 		panelVoltar.setContentAreaFilled(false);
 		panelVoltar.setBorderPainted(false);
 
@@ -84,8 +174,8 @@ public class ModoDeJogo extends JPanel {
 	}
 
 	/**
-	 * Tratando eventos de clique, � uma �rea de escolha de dois modos de jogo
-	 * ent�o ao clicar em uma op��o, a configura��o deve alterar e a tela tamb�m
+	 * Tratando eventos de clique, é uma área de escolha de dois modos de jogo
+	 * então ao clicar em uma opção, a configuração deve alterar e a tela também
 	 */
 	private void eventos() {
 		panelFacil.addMouseListener(new MouseListener() {
