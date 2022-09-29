b813114e0cd681a3b182bf6f930d1f0667017b55
Kauan Ferreira dos Santos <113863647+KauanSntz@users.noreply.github.com>
2022-09-29 11:35:16 -0400

    Adicionando componentes

--- Dindin/src/ModoDificil.java
+++ Dindin/src/ModoDificil.java
@@ -1,91 +1,156 @@
-/**AndrÈ Kaled Duarte - 10/09/2022
+package view;
+
+/**Andr√© Kaled Duarte - 28/09/2022
  * 
- * Modo dificil
- * essa classe deve ser um dos modos de jogo escolhido pelo usu·rio, as configurÁıes do jogo s„o
- * alteradas porÈm a interface continua a mesma, aqui È onde ser· a area principal do jogo pois
- * È onde os jogadores passar„o mais tempo.
+ * Modo f√°cil
+ * essa classe deve ser um dos modos de jogo escolhido pelo usu√°rio, as configur√ß√µes do jogo s√£o
+ * definidas para iniciantes por√©m a interface continua a mesma, aqui √© onde ser√° a area principal do jogo pois
+ * √© onde os jogadores passar√£o mais tempo.
+ * 
  * Dado? ok
+ * Vez do jogador a mostra? ok
  * Tabuleiro? X
- * Conectado posiÁ„o do personagem? X
+ * Conectado posi√ß√£o do personagem? X
  * Lista de compras do Jogador? X
  * */
-
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import modelo.Dado;
 
-public class ModoDificil extends JPanel{
+public class ModoDificil extends JPanel {
 
-	//declaraÁ„o dos componentes
-	private JPanel areaDado;
-	private JButton btRolar,btVoltar;
-	private JLabel resultado;
-	private static String nome = "MODO DIFICIL";
-	
-	//construtor da classe
-	public ModoDificil(){
+	private JPanel areaDado, painelVez;
+	private JButton btRolar, btVoltar, btLista,btMenu;
+	private JLabel resultado, areaVez, moeda, quantMoeda, personagem, nome;
+	ImageIcon imgFundo = new ImageIcon(getClass().getResource("area-jogador-da-vez.png")),
+			imgLista = new ImageIcon(getClass().getResource("bot√£o lista.png")),
+			imgMoeda = new ImageIcon(getClass().getResource("moeda-java.png")),
+			imgPersonagem = new ImageIcon(getClass().getResource("personagem.png")),
+			imgNome = new ImageIcon(getClass().getResource("nome.png")),
+			imgMenu = new ImageIcon(getClass().getResource("botao-menu.png"));
+	//Font font = Font.createFont(Font.TRUETYPE_FONT, new FileInputStream(getClass().getResource("VCR_OSD_MONO_1.001.ttf")));
+
+	public ModoDificil() {
 		setLayout(null);
-		setBackground(new Color(66, 153, 206));
-		setName(nome);
-		
-		//instanciaÁ„o
+		setBackground(new Color(200, 133, 238));
+		setName("MODO FACIL");
 		areaDado = new JPanel();
 		btRolar = new JButton("ROLAR DADO");
 		btVoltar = new JButton("VOLTAR");
-		resultado = new JLabel("" +Dado.rolar());
-		
+		resultado = new JLabel("" + Dado.rolar());
+		btMenu = new JButton(imgMenu);
+
 		configuraDado();
 		configuraBtVoltar();
+		configuraVez();
+		configuraBtMenu();
 	}
-	
-	//configurando area do Dado
-	private void configuraDado(){
+
+	// configurando a aba de ver a vez do jogador
+	private void configuraVez() {
+		painelVez = new JPanel();
+		painelVez.setLayout(null);
+		painelVez.setBounds(508, 550, 350, 200);
+		add(painelVez);
+		painelVez.setOpaque(false);
+
+		areaVez = new JLabel(imgFundo);
+		// areaVez.setOpaque(true);
+		areaVez.setBounds(painelVez.getBounds());
+
+		btLista = new JButton(imgLista);
+		btLista.setBorderPainted(false);
+		btLista.setFocusable(false);
+		btLista.setContentAreaFilled(false);
+		btLista.setBounds(100, 40, 62, 62);
+
+		moeda = new JLabel(imgMoeda);
+		moeda.setBounds(150, 40, 70, 70);
+		
+		personagem = new JLabel(imgPersonagem);
+		personagem.setBounds(7, 10, 100, 100);
+		
+		nome = new JLabel(imgNome);
+		nome.setBounds(110, -70, 200, 200);
+
+		quantMoeda = new JLabel("XXX,XX");
+		int x = moeda.getX();
+		int y = moeda.getY();
+		int w = moeda.getWidth();
+		int h = moeda.getHeight();
+		quantMoeda.setBounds(x + 60, y + 20, w, h - 50);
+		//quantMoeda.setFont(font);
+
+		add(areaVez);
+		painelVez.add(btLista);
+		painelVez.add(moeda);
+		painelVez.add(quantMoeda);
+		painelVez.add(personagem);
+		painelVez.add(nome);
+	}
+
+	// configurando area do Dado
+	private void configuraDado() {
 		areaDado.setBackground(Color.GRAY);
 		areaDado.setLayout(new FlowLayout());
 		areaDado.add(resultado);
 		areaDado.add(btRolar);
-		areaDado.setBounds(100,100,100,60);
-		
+		areaDado.setBounds(100, 100, 100, 100);
+		add(areaDado);
+
 		resultado.setFont(new Font("Arial", 1, 19));
 		resultado.setForeground(Color.ORANGE);
 		btRolar.setFont(new Font("Arial", 1, 14));
-		
-		add(areaDado);
-		
-		btRolar.addActionListener(new ActionListener(){
+
+		btRolar.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				resultado.setText("" +Dado.rolar());
+				resultado.setText("" + Dado.rolar());
 			}
-			
+
 		});
 	}
 
-	//configurando botao de voltar
-		private void configuraBtVoltar(){
-			btVoltar.setBounds(10,10,100,20);
-			add(btVoltar);
-			btVoltar.addActionListener(new ActionListener(){
+	// configurando botao de voltar
+	private void configuraBtVoltar() {
+		//btVoltar.setBounds(10, 10, 100, 20);
+		add(btVoltar);
+		btVoltar.addActionListener(new ActionListener() {
 
-				@Override
-				public void actionPerformed(ActionEvent arg0) {
-					Menu.mudaTela(ModoDeJogo.getNome());
-					
-				}
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				Menu.voltaTela();
+
+			}
+
+		});
+	}
+	
+	private void configuraBtMenu() {
+		btMenu.setBounds(1220, 30, 100, 105);
+		btMenu.setBorderPainted(false);
+		btMenu.setFocusable(false);
+		btMenu.setContentAreaFilled(false);
+		add(btMenu);
+		btMenu.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
 				
-			});
-		}
-		
-	//metodo de acesso a informaÁıes da classe
-	static String getNome(){
-		return nome;
+			}
+			
+			
+		});
 	}
 }
