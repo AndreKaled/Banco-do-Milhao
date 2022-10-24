7140fa53fbc93e32550d97cdf1188a4989917569
grzlrlph <grazi.ralph@gmail.com>
2022-10-24 00:22:33 -0400

    dadinho ajeitado

--- Dindin/src/view/ModoFacil.java
+++ Dindin/src/view/ModoFacil.java
@@ -1,10 +1,11 @@
 package view;
 
-/**Andr� Kaled Duarte - 30/09/2022
+/**Andre Kaled Duarte - 30/09/2022
  * Sarah Pinheiro Antunes - 29/09/2022
  * Graziela da Costa Ralph - 16/10/2022
  * Sarah Pinheiro Antunes - 17/10/2022
  * Graziela da Costa Ralph - 17/10/2022
+ * Graziela da Costa Ralph - 23/10/2022
  * 
  * Modo facil
  * essa classe deve ser um dos modos de jogo escolhido pelo usu�rio, as configur��es do jogo s�o
@@ -18,8 +19,6 @@ package view;
  * Lista de compras do Jogador? X
  * */
 import java.awt.Color;
-import java.awt.FlowLayout;
-import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -34,8 +33,7 @@ public class ModoFacil extends JPanel {
 
 	private JPanel areaDado, painelVez;
 	private JButton btRolar, btVoltar, btLista, btMenu;
-	private JLabel resultado1, resultado2, resultado3, resultado4, resultado5,
-			resultado6, areaVez, moeda, quantMoeda, personagem, nome;
+	private JLabel resultado, areaVez, moeda, quantMoeda, personagem, nome, lbDado;
 	private int rolaDado = 0, dadoAntigo = 0;
 	ImageIcon imgFundo = new ImageIcon("Imagens/area-jogador-da-vez.png"),
 			imgLista = new ImageIcon("Imagens/imagens/botao lista.png"),
@@ -43,13 +41,10 @@ public class ModoFacil extends JPanel {
 			imgPersonagem = new ImageIcon("Imagens/personagem.png"),
 			imgNome = new ImageIcon("Imagens/nome.png"),
 			imgMenu = new ImageIcon("Imagens/botao-menu.png"),
-			imgRolar = new ImageIcon("Imagens/botao-rola-dado.png"),
-			imgDado1 = new ImageIcon("Imagens/Dado-1.png"),
-			imgDado2 = new ImageIcon("Imagens/Dado-2.png"),
-			imgDado3 = new ImageIcon("Imagens/Dado-3.png"),
-			imgDado4 = new ImageIcon("Imagens/Dado-4.png"),
-			imgDado5 = new ImageIcon("Imagens/Dado-5.png"),
-			imgDado6 = new ImageIcon("Imagens/Dad-6.png");
+			imgVoltar = new ImageIcon ("Imagens/botao voltar.png"),
+			imgRolar = new ImageIcon("Imagens/botao-rola-dado.png");
+	ImageIcon DadoIcon;
+	private String imgDado[] = {"1", "2", "3", "4", "5", "6"};
 
 	public ModoFacil() {
 		setLayout(null);
@@ -57,15 +52,13 @@ public class ModoFacil extends JPanel {
 		setName("MODO FACIL");
 		areaDado = new JPanel();
 		btRolar = new JButton(imgRolar);
-		btVoltar = new JButton("VOLTAR");
-		resultado1 = new JLabel(imgDado1);
-		resultado2 = new JLabel(imgDado2);
-		resultado3 = new JLabel(imgDado3);
-		resultado4 = new JLabel(imgDado4);
-		resultado5 = new JLabel(imgDado5);
-		resultado6 = new JLabel(imgDado6);
+		btVoltar = new JButton(imgVoltar);
+		resultado = new JLabel(imgDado[0]);
 		btMenu = new JButton(imgMenu);
 
+		DadoIcon= new ImageIcon(imgDado[0]+".png"); //acessa as imagens
+        lbDado = new JLabel(DadoIcon);
+		
 		configuraDado();
 		configuraBtVoltar();
 		configuraVez();
@@ -117,195 +110,37 @@ public class ModoFacil extends JPanel {
 
 	// configurando area do Dado
 	private void configuraDado() {
-
+		
 		// config para o painel
 		areaDado.setLayout(null);
 		areaDado.setOpaque(false);
 
 		// adicionando os componentes
-		areaDado.add(resultado1);
-		areaDado.add(resultado2);
-		areaDado.add(resultado3);
-		areaDado.add(resultado4);
-		areaDado.add(resultado5);
-		areaDado.add(resultado6);
 		areaDado.add(btRolar);
+		areaDado.add(lbDado);
 		add(areaDado);
 
 		// posicionamento e tamanho
 		btRolar.setBounds(30, 200, 150, 150);
-		resultado1.setBounds(-100, -70, 400, 500);
-		resultado2.setBounds(-100, -70, 400, 500);
-		resultado3.setBounds(-100, -70, 400, 500);
-		resultado4.setBounds(-100, -70, 400, 500);
-		resultado5.setBounds(-100, -70, 400, 500);
-		resultado6.setBounds(-100, -70, 400, 500);
+		lbDado.setBounds(-100, -70, 400, 500);
 		areaDado.setBounds(1000, 350, 200, 400);
 
-		// bagulhin do setvisible
-		resultado1.setVisible(true);
-		resultado2.setVisible(false);
-		resultado3.setVisible(false);
-		resultado4.setVisible(false);
-		resultado5.setVisible(false);
-		resultado6.setVisible(false);
 
-		// configuraçao do botao rolar dado
+
+		// configuraçao da area do botao rolar dado
 		btRolar.setContentAreaFilled(false);
 		btRolar.setBorderPainted(false);
 
-		/*
-		 * resultado.setFont(new Font("Arial", 1, 19));
-		 * resultado.setForeground(Color.ORANGE); btRolar.setFont(new
-		 * Font("Arial", 1, 14));
-		 */
-
+	
 		btRolar.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				rolaDado = Dado.rolar();
-				switch (rolaDado) {
-				case 1:
-					switch (dadoAntigo) {
-					case 1:
-						break;
-					case 2:
-						resultado2.setVisible(false);
-						break;
-					case 3:
-						resultado3.setVisible(false);
-						break;
-					case 4:
-						resultado4.setVisible(false);
-						break;
-					case 5:
-						resultado5.setVisible(false);
-						break;
-					case 6:
-						resultado6.setVisible(false);
-						break;
-					}
-					resultado1.setVisible(true);
-					dadoAntigo = rolaDado;
-					break;
-				case 2:
-					switch (dadoAntigo) {
-					case 1:
-						resultado1.setVisible(false);
-						break;
-					case 2:
-						break;
-					case 3:
-						resultado3.setVisible(false);
-						break;
-					case 4:
-						resultado4.setVisible(false);
-						break;
-					case 5:
-						resultado5.setVisible(false);
-						break;
-					case 6:
-						resultado6.setVisible(false);
-						break;
-					}
-					resultado2.setVisible(true);
-					dadoAntigo = rolaDado;
-					break;
-				case 3:
-					switch (dadoAntigo) {
-					case 1:
-						resultado1.setVisible(false);
-						break;
-					case 2:
-						resultado2.setVisible(false);
-						break;
-					case 3:
-						break;
-					case 4:
-						resultado4.setVisible(false);
-						break;
-					case 5:
-						resultado5.setVisible(false);
-						break;
-					case 6:
-						resultado6.setVisible(false);
-						break;
-					}
-					resultado3.setVisible(true);
-					dadoAntigo = rolaDado;
-					break;
-				case 4:
-					switch (dadoAntigo) {
-					case 1:
-						resultado1.setVisible(false);
-						break;
-					case 2:
-						resultado2.setVisible(false);
-						break;
-					case 3:
-						resultado3.setVisible(false);
-						break;
-					case 4:
-						break;
-					case 5:
-						resultado5.setVisible(false);
-						break;
-					case 6:
-						resultado6.setVisible(false);
-						break;
-					}
-					resultado4.setVisible(true);
-					dadoAntigo = rolaDado;
-					break;
-				case 5:
-					switch (dadoAntigo) {
-					case 1:
-						resultado1.setVisible(false);
-						break;
-					case 2:
-						resultado2.setVisible(false);
-						break;
-					case 3:
-						resultado3.setVisible(false);
-						break;
-					case 4:
-						resultado4.setVisible(false);
-						break;
-					case 5:
-						break;
-					case 6:
-						resultado6.setVisible(false);
-						break;
-					}
-					resultado5.setVisible(true);
-					dadoAntigo = rolaDado;
-					break;
-				case 6:
-					switch (dadoAntigo) {
-					case 1:
-						resultado1.setVisible(false);
-						break;
-					case 2:
-						resultado2.setVisible(false);
-						break;
-					case 3:
-						resultado3.setVisible(false);
-						break;
-					case 4:
-						resultado4.setVisible(false);
-						break;
-					case 5:
-						resultado5.setVisible(false);
-						break;
-					case 6:
-						break;
-					}
-					resultado6.setVisible(true);
-					dadoAntigo = rolaDado;
-					break;
-				}
-
+				int i = Dado.rolar();
+		
+				DadoIcon = new ImageIcon("Imagens/Dado-"+imgDado[i]+".png"); // acessa o nome da imagem
+	            lbDado.setIcon(DadoIcon);
+				
 			}
 
 		});
@@ -313,7 +148,9 @@ public class ModoFacil extends JPanel {
 
 	// configurando botao de voltar
 	private void configuraBtVoltar() {
-		// btVoltar.setBounds(10, 10, 100, 20);
+		btVoltar.setBounds(10, 10, 70, 70);
+		btVoltar.setContentAreaFilled(false);
+		btVoltar.setBorderPainted(false);
 		add(btVoltar);
 		btVoltar.addActionListener(new ActionListener() {
 
